b85233d550cdacada045838bc245f940869618f5
Elyan <ekenimohelyanpheta@gmail.com>
2023-02-15 12:35:46 -0800

    update exercise and solve hash challenge

--- hashmaps/hashmaps1.rs
+++ hashmaps/hashmaps1.rs
@@ -15,10 +15,13 @@
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();// TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
+    basket.insert(String::from("apple"), 4);
+    basket.insert(String::from("orange"), 3);
+    basket.insert(String::from("cashew"), 6);
 
     // TODO: Put more fruits in your basket here.
 
